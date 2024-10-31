aa1d994d6b92f15b4578ef5e745ff88bf1963a6b
Anwar Rahim <anwargithub@gmail.com>
2024-10-31 22:52:44 +0500

    almost design unit converter

--- script.js
+++ script.js
@@ -7,12 +7,34 @@
 
 const inputEl = document.getElementById("input-el")
 const convertBtn = document.getElementById("convert-btn")
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
+
 
 
 convertBtn.addEventListener("click", function(){
+   
+   
+   
     let inputNum =Number(inputEl.value)
-    console.log(inputNum)
-    console.log(typeof inputNum)
+    // Conversion length
+    if(inputNum!=0) {
+        let meterIntoFeet = (inputNum * 3.281).toFixed(3)
+        let feetIntoMeter = (3.281 / inputNum).toFixed(3)
+        lengthEl.textContent = `${inputNum} meters = ${meterIntoFeet} feets | ${inputNum} feets = ${feetIntoMeter} meters`
+        // Conversion length
+        let letterIntoGallon = (inputNum * 0.264).toFixed(3)
+        let gallonIntoLetter = (0.264 / inputNum).toFixed(3)
+        volumeEl.textContent = `${inputNum} liter = ${letterIntoGallon} gallons | ${inputNum} Gallon = ${gallonIntoLetter} liters`
+        // Conversion mass
+        let kiloIntoPound = (inputNum * 2.204).toFixed(3)
+        let poundIntoKilo = (2.204 / inputNum).toFixed(3)
+        
+        massEl.textContent = `${inputNum} Kilo = ${kiloIntoPound} pounds | ${inputNum} Pound = ${poundIntoKilo} kilos`
+    }
+    
 })
+
 
 
